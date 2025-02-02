a6414d9b604c0554da04ce007fcd958e7328b3cf
naca-nyan <naca.nyan@gmail.com>
2025-02-02 12:51:51 +0900

    Use popup action instead of options_page

--- content.js
+++ content.js
@@ -1,46 +1,58 @@
-chrome.storage.sync.get(['chordstyleOption', 'wordstyleOption', 'greenbackOption', 'columnCount', 'compactOption'], function(item) {
-  const chordstyleOption = item.chordstyleOption || 'bold';
-  const wordstyleOption = item.wordstyleOption || 'normal';
-  const greenbackOption = item.greenbackOption || 'disable';
-  const columnCount = item.columnCount || '1';
-  const compactOption = item.compactOption || false;
-  let chordfontSize, chordfontWeight, chordtop, lineHeight;
-  let wordfontWeight;
-  let background;
+function applyStyleFromStorage() {
+  chrome.storage.sync.get(['chordstyleOption', 'wordstyleOption', 'greenbackOption', 'columnCount', 'compactOption'], function(item) {
+    const chordstyleOption = item.chordstyleOption || 'bold';
+    const wordstyleOption = item.wordstyleOption || 'normal';
+    const greenbackOption = item.greenbackOption || 'disable';
+    const columnCount = item.columnCount || '1';
+    const compactOption = item.compactOption || false;
+    let chordfontSize, chordfontWeight, chordtop, lineHeight;
+    let wordfontWeight;
+    let background;
 
-  switch(chordstyleOption) {
-      case 'normal':
-          chordfontSize = 'smaller';
-          chordfontWeight = 'normal';
-          chordtop = '-1.5em';
-          lineHeight = '1.5em';
-          break;
-      case 'bold':
-          chordfontSize = 'smaller';
-          chordfontWeight = 'bold';
-          chordtop = '-1.5em';
-          lineHeight = '1.5em';
-          break;
-      case 'bold-large':
-          chordfontSize = '0.95em';
-          chordfontWeight = 'bold';
-          chordtop = '-1.3em';
-          lineHeight = '1.9em';
-          break;
-      case 'bold-verylarge':
-          chordfontSize = '1.05em';
-          chordfontWeight = 'bold';
-          chordtop = '-1.2em';
-          lineHeight = '2.1em';
-          break;
-      default:
-          chordfontSize = 'smaller';
-          chordfontWeight = 'bold';
-          chordtop = '-1.5em';
-          lineHeight = '1.5em';
-  }
+    switch(chordstyleOption) {
+        case 'normal':
+            chordfontSize = 'smaller';
+            chordfontWeight = 'normal';
+            chordtop = '-1.5em';
+            lineHeight = '1.5em';
+            break;
+        case 'bold':
+            chordfontSize = 'smaller';
+            chordfontWeight = 'bold';
+            chordtop = '-1.5em';
+            lineHeight = '1.5em';
+            break;
+        case 'bold-large':
+            chordfontSize = '0.95em';
+            chordfontWeight = 'bold';
+            chordtop = '-1.3em';
+            lineHeight = '1.9em';
+            break;
+        case 'bold-verylarge':
+            chordfontSize = '1.05em';
+            chordfontWeight = 'bold';
+            chordtop = '-1.2em';
+            lineHeight = '2.1em';
+            break;
+        default:
+            chordfontSize = 'smaller';
+            chordfontWeight = 'bold';
+            chordtop = '-1.5em';
+            lineHeight = '1.5em';
+    }
 
-  switch(wordstyleOption) {
+    switch(wordstyleOption) {
+        case 'normal':
+            wordfontWeight = 'normal';
+            break;
+        case 'bold':
+            wordfontWeight = 'bold';
+            break;
+        default:
+            wordfontWeight = 'normal';
+    }
+
+    switch(wordstyleOption) {
       case 'normal':
           wordfontWeight = 'normal';
           break;
@@ -49,97 +61,92 @@ chrome.storage.sync.get(['chordstyleOption', 'wordstyleOption', 'greenbackOption
           break;
       default:
           wordfontWeight = 'normal';
-  }
+    }
 
-  switch(wordstyleOption) {
-    case 'normal':
-        wordfontWeight = 'normal';
-        break;
-    case 'bold':
-        wordfontWeight = 'bold';
-        break;
-    default:
-        wordfontWeight = 'normal';
-  }
+    switch(greenbackOption) {
+      case 'disable':
+          background = '#FFFFFF';
+          break;
+      case 'enable':
+          background = '#00902A';
+          break;
+      default:
+          background = '#FFFFFF';
+    }
 
-  switch(greenbackOption) {
-    case 'disable':
-        background = '#FFFFFF';
-        break;
-    case 'enable':
-        background = '#00902A';
-        break;
-    default:
-        background = '#FFFFFF';
-  }
-
-  document.querySelectorAll('span.chord').forEach(function(chord) {
-      chord.style.fontSize = chordfontSize;
-      chord.style.fontWeight = chordfontWeight;
-      chord.style.top = chordtop;
-  });
-
-  document.querySelectorAll('span.word').forEach(function(word) {
-      word.style.fontWeight = wordfontWeight;
-  });
-
-  document.querySelectorAll('span.wordtop').forEach(function(word) {
-      word.style.fontWeight = wordfontWeight;
-  });
-
-  document.querySelectorAll('div.main').forEach(function(div) {
-      div.style.lineHeight = lineHeight;
-  });
-
-  document.body.style.backgroundColor = background;
-
-  const styleSheet = new CSSStyleSheet();
-  styleSheet.replaceSync(`div.main div { column-count: ${columnCount}; }`);
-  document.adoptedStyleSheets = [styleSheet];
-
-  if (compactOption) {
-    // Wrap every `span.chord` and `span.word` pair with `span.chordword`
-    document.querySelectorAll(".main .line").forEach(function(line) {
-      const p = document.createElement("p");
-      while (line.children.length > 0) {
-        const chord = line.children[0];
-        const word = line.children[1];
-        if (chord.classList.contains("chord") && 
-            word && word.classList.contains("word")) {
-          const span = document.createElement("span");
-          span.classList = "chordword";
-          span.appendChild(chord);
-          span.appendChild(word);
-          p.appendChild(span);
-        } else {
-          p.appendChild(chord);
-        }
-      }
-      line.innerHTML = p.innerHTML;
+    document.querySelectorAll('span.chord').forEach(function(chord) {
+        chord.style.fontSize = chordfontSize;
+        chord.style.fontWeight = chordfontWeight;
+        chord.style.top = chordtop;
     });
+
+    document.querySelectorAll('span.word').forEach(function(word) {
+        word.style.fontWeight = wordfontWeight;
+    });
+
+    document.querySelectorAll('span.wordtop').forEach(function(word) {
+        word.style.fontWeight = wordfontWeight;
+    });
+
+    document.querySelectorAll('div.main').forEach(function(div) {
+        div.style.lineHeight = lineHeight;
+    });
+
+    document.body.style.backgroundColor = background;
+
     const styleSheet = new CSSStyleSheet();
-    styleSheet.replaceSync(`
-      div.main span.chordword {
-        display: inline-flex;
-        flex-direction: column;
-      }
-      div.main span.chord, div.main span.word, div.main span.wordtop {
-        position: static;
-        line-height: 1em;
-      }
-      div.main span.wordtop {
-        vertical-align: bottom;
-      }
-      div.main span.chord {
-        margin-right: 6px;
-      }
-      div.main span.word {
-        white-space: pre;
-      }
-      div.main p.line {
-        margin-block: 0 .3em;
-      }
-    `);
-    document.adoptedStyleSheets.push(styleSheet);
-  }
+    styleSheet.replaceSync(`div.main div { column-count: ${columnCount}; }`);
+    document.adoptedStyleSheets = [styleSheet];
+
+    if (compactOption) {
+      // Wrap every `span.chord` and `span.word` pair with `span.chordword`
+      document.querySelectorAll(".main .line").forEach(function(line) {
+        const p = document.createElement("p");
+        while (line.children.length > 0) {
+          const chord = line.children[0];
+          const word = line.children[1];
+          if (chord.classList.contains("chord") &&
+              word && word.classList.contains("word")) {
+            const span = document.createElement("span");
+            span.classList = "chordword";
+            span.appendChild(chord);
+            span.appendChild(word);
+            p.appendChild(span);
+          } else {
+            p.appendChild(chord);
+          }
+        }
+        line.innerHTML = p.innerHTML;
+      });
+      const styleSheet = new CSSStyleSheet();
+      styleSheet.replaceSync(`
+        div.main span.chordword {
+          display: inline-flex;
+          flex-direction: column;
+        }
+        div.main span.chord, div.main span.word, div.main span.wordtop {
+          position: static;
+          line-height: 1em;
+        }
+        div.main span.wordtop {
+          vertical-align: bottom;
+        }
+        div.main span.chord {
+          margin-right: 6px;
+        }
+        div.main span.word {
+          white-space: pre;
+        }
+        div.main p.line {
+          margin-block: 0 .3em;
+        }
+      `);
+      document.adoptedStyleSheets.push(styleSheet);
+    }
+  });
+}
+
+applyStyleFromStorage();
+chrome.storage.onChanged.addListener(function() {
+  applyStyleFromStorage();
 });
