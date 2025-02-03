31a053e6281ff0005e9e42edbd59999ae47b696f
naca-nyan <naca.nyan@gmail.com>
2025-02-04 00:38:20 +0900

    Improve UI for chord & word style option

--- options.js
+++ options.js
@@ -1,7 +1,19 @@
+function getRadioValue(name) {
+    for (const e of document.getElementsByName(name)) {
+        if (e.checked) return e.value;
+    }
+    return null;
+}
+function setRadioValue(name, value) {
+    for (const e of document.getElementsByName(name)) {
+        if (e.value == value) return e.checked = true;
+    }
+    return false;
+}
 // Save the options
 function saveOptions() {
-    const chordstyleOption = document.getElementById('chordstyleOption').value;
-    const wordstyleOption = document.getElementById('wordstyleOption').value;
+    const chordstyleOption = getRadioValue('chordstyleOption') ?? 'bold';
+    const wordstyleOption = getRadioValue('wordstyleOption') ?? 'normal';
     const greenbackOption = document.getElementById('greenbackOption').value;
     const columnCount = document.getElementById('columnCount').value;
     const compactOption = document.getElementById('compactOption').checked;
@@ -9,8 +21,8 @@ function saveOptions() {
         console.log('Option saved.');
     });
 }
-document.getElementById('chordstyleOption').onchange = saveOptions;
-document.getElementById('wordstyleOption').onchange = saveOptions;
+document.getElementsByName('chordstyleOption').forEach((e) => e.onchange = saveOptions);
+document.getElementsByName('wordstyleOption').forEach((e) => e.onchange = saveOptions);
 document.getElementById('greenbackOption').onchange = saveOptions;
 document.getElementById('columnCount').onchange = saveOptions;
 document.getElementById('compactOption').onchange = saveOptions;
@@ -18,8 +30,8 @@ document.getElementById('compactOption').onchange = saveOptions;
 // Load the saved options
 document.addEventListener('DOMContentLoaded', function() {
     chrome.storage.sync.get(['chordstyleOption', 'wordstyleOption', 'greenbackOption', 'columnCount', 'compactOption'], function(item) {
-        document.getElementById('chordstyleOption').value = item.chordstyleOption || 'bold';
-        document.getElementById('wordstyleOption').value = item.wordstyleOption || 'normal';
+        setRadioValue('chordstyleOption', item.chordstyleOption || 'bold');
+        setRadioValue('wordstyleOption', item.wordstyleOption || 'normal');
         document.getElementById('greenbackOption').value = item.greenbackOption || 'disable';
         document.getElementById('columnCount').value = item.columnCount || '1';
         document.getElementById('compactOption').checked = item.compactOption || false;
