824b075bd0431705ea3a8f34f4547d7ab718255c
naca-nyan <naca.nyan@gmail.com>
2025-02-04 21:47:28 +0900

    Improve UI for compact, columncount and background

--- options.js
+++ options.js
@@ -14,26 +14,26 @@ function setRadioValue(name, value) {
 function saveOptions() {
     const chordstyleOption = getRadioValue('chordstyleOption') ?? 'bold';
     const wordstyleOption = getRadioValue('wordstyleOption') ?? 'normal';
-    const greenbackOption = document.getElementById('greenbackOption').value;
+    const compactOption = getRadioValue('compactOption') == 'compact';
+    const greenbackOption = getRadioValue('greenbackOption') == 'green' ? 'enable' : 'disable';
     const columnCount = document.getElementById('columnCount').value;
-    const compactOption = document.getElementById('compactOption').checked;
     chrome.storage.sync.set({chordstyleOption, wordstyleOption, greenbackOption, columnCount, compactOption}, function() {
         console.log('Option saved.');
     });
 }
 document.getElementsByName('chordstyleOption').forEach((e) => e.onchange = saveOptions);
 document.getElementsByName('wordstyleOption').forEach((e) => e.onchange = saveOptions);
-document.getElementById('greenbackOption').onchange = saveOptions;
-document.getElementById('columnCount').onchange = saveOptions;
-document.getElementById('compactOption').onchange = saveOptions;
+document.getElementsByName('compactOption').forEach((e) => e.onchange = saveOptions);
+document.getElementsByName('greenbackOption').forEach((e) => e.onchange = saveOptions);
+document.getElementById('columnCount').oninput = saveOptions;
 
 // Load the saved options
 document.addEventListener('DOMContentLoaded', function() {
     chrome.storage.sync.get(['chordstyleOption', 'wordstyleOption', 'greenbackOption', 'columnCount', 'compactOption'], function(item) {
         setRadioValue('chordstyleOption', item.chordstyleOption || 'bold');
         setRadioValue('wordstyleOption', item.wordstyleOption || 'normal');
-        document.getElementById('greenbackOption').value = item.greenbackOption || 'disable';
+        setRadioValue('compactOption', item.compactOption ? 'compact' : 'normal');
+        setRadioValue('greenbackOption', item.greenbackOption == 'enabled' ? 'green' : 'white');
         document.getElementById('columnCount').value = item.columnCount || '1';
-        document.getElementById('compactOption').checked = item.compactOption || false;
     });
 });
